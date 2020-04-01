3389bb90beed44a8110117745eb47336573b5239
Helen <helen@Helens-MBP.fritz.box>
2020-04-01 15:26:42 +0200

    composer & npm init

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AwStudio\Fjord;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
